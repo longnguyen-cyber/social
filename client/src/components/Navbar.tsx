5ecd78690fc52e3bf50b76d9b22cbad795c55fa2
longnguyen-cyber <longnguyendev2020@gmail.com>
2022-06-11 21:17:07 +0700

    done

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,10 +1,15 @@
 import { SearchIcon } from '@heroicons/react/solid'
 import { BiMessageRoundedDetail } from 'react-icons/bi'
 import { BsBell } from 'react-icons/bs'
+import { IoMdArrowDropdown } from 'react-icons/io'
+import { Link, Outlet } from 'react-router-dom'
 function Navbar() {
+  const id = 3
   return (
     <div className="grid grid-cols-3 bg-cyan-400 py-2 px-4">
-      <h3 className="text-2xl font-medium text-white">Social</h3>
+      <Link to="/">
+        <h3 className="text-2xl font-medium text-white">Social</h3>
+      </Link>
       <form className="bg-white rounded-3xl flex items-center p-2">
         <button type="submit">
           <SearchIcon className="h-6 mx-2" />
@@ -16,20 +21,31 @@ function Navbar() {
         />
       </form>
       <div className="flex items-center space-x-2 ml-auto cursor-pointer">
-        <div className="flex items-center space-x-2 relative group profile">
-          <img
-            src="https://source.unsplash.com/random"
-            alt=""
-            className="h-9 w-9 rounded-full"
-          />
+        <div className="flex items-center space-x-2">
+          <Link to={id.toString()}>
+            <img
+              src="https://source.unsplash.com/random"
+              alt=""
+              className="h-9 w-9 rounded-full"
+            />
+          </Link>
           <span className="font-semibold text-sm text-white cursor-text">
             kuga
           </span>
-
-          <button className="logout">logout</button>
+          <div className="group relative more">
+            <IoMdArrowDropdown className="text-2xl" />
+            <Link
+              to="/login"
+              className="logout"
+              onClick={() => localStorage.removeItem('user')}
+            >
+              logout
+            </Link>
+          </div>
         </div>
         <BiMessageRoundedDetail className="h-10 mx-2 text-2xl" />
         <BsBell className="h-10 mx-2 text-2xl" />
+        <Outlet />
       </div>
     </div>
   )
